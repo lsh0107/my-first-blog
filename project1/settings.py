623b652fe37a65806f1d8b27a74531a29889f37f
lsh0107 <zippoo94@naver.com>
2023-05-16 16:16:29 +0900

    small changes

--- project1/settings.py
+++ project1/settings.py
@@ -122,3 +122,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'

07c1149ebed3ed2aca43979236fa0072d0b6abea
lsh0107 <zippoo94@naver.com>
2023-05-16 16:04:12 +0900

    changed HTML for the site

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -6,11 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+#from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+
+
 
 application = get_wsgi_application()
